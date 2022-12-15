04b8092329a53981071bc6d0bceaa063c29fcea0
Diego <diego.mastella01@universitadipavia.it>
2022-12-15 11:56:19 +0000

    Passing files to VM

--- substringSearch.c
+++ substringSearch.c
@@ -1,26 +1,27 @@
-#include <stdio.h>
+g#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <mpi.h>
 
-char * file_read(FILE *file,int starting_point, int dim);
-
-struct pos_s{
+typedef struct{
   int starting_point;
   int dim;
-} pos;
+}pos;
+
+char * file_read(FILE *file, pos p);
 
 void main(int argc, char **argv) {
 
   MPI_Status status;
-  int myrank, size, starting_point;
-  FILE *file_in;
-  file_in = fopen("genoma-test.txt","r");
-
+  int myrank, size;
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD,&myrank);
   MPI_Comm_size(MPI_COMM_WORLD,&size);
 
+
+  FILE *file_in;
+  int starting_point;
+  file_in = fopen("genoma-test.txt","r");
   const int    nitems=2;
   int          blocklengths[2] = {1,1};
   MPI_Datatype types[2] = {MPI_INT, MPI_INT};
@@ -34,45 +35,68 @@ void main(int argc, char **argv) {
 
   if(myrank == 0) {
 
-    int dim = SEEK_SET - SEEK_END;
-    pos *p;
-    p->dim = dim/size;
+    fseek(file_in, 0, SEEK_END);
+    int dim = ftell(file_in);
+    rewind(file_in);
+    pos p;
+    p.dim = dim/size;
     int resto = dim%size;
 
-
+    char *seq;
 
     for(int i=1;i<size;i++) {
 
-      p->starting_point = (dim_per_cpu*i)+resto;
-      MPI_Send(p, 1, mpi_pos_type, i+1, 555, MPI_COMM_WORLD);
+      p.starting_point = (p.dim*i)+resto;
+      printf("proc%d : %d\n", i, p.starting_point);
+      MPI_Send(&p, sizeof(p), mpi_pos_type, i, 555, MPI_COMM_WORLD);
     }
 
+    p.starting_point = 0;
+    p.dim = p.dim+resto;
+
+    printf("proc%d : dim:%d start:%d\n", myrank, p.dim, p.starting_point);
+
+    seq = file_read(file_in, p);
+
+    printf("proc%d : %s", myrank, seq);
+
+
+    free(seq);
 
 
 
   } else{
 
-    char seq[DIM];
+    char *seq;
+    pos p;
 
-    MPI_Recv(&starting_point, 1, MPI_INT, 0, 555, MPI_COMM_WORLD, &status);
+    MPI_Recv(&p, sizeof(p), mpi_pos_type, 0, 555, MPI_COMM_WORLD, &status);
+    printf("proc%d : dim:%d start:%d\n", myrank, p.dim, p.starting_point);
 
-    file_read(file_in, starting_point, seq);
+    seq = file_read(file_in, p);
+
+
+    free(seq);
+
 
   }
 
-
   fclose(file_in);
-
   MPI_Finalize();
 
 }
 
-char * file_read(FILE *file,int starting_point, int dim) {
+char * file_read(FILE *file, pos p) {
 
-  char seq[dim];
+  char *seq;
+  seq = malloc(p.dim * sizeof(char));
 
-  fseek(file,starting_point,SEEK_SET);
+  fseek(file,0,p.starting_point);
 
+  for(int i=0; i<p.dim; i++) {
 
+    seq[i] = fgetc(file);
+
+  }
 
 }
