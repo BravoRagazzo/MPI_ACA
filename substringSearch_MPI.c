30008e2cdd6cec40058a935725db0357021175cc
Diego <diego.mastella01@universitadipavia.it>
2022-12-20 10:11:47 +0100

    added overlapping logic for searching

--- substringSearch_MPI.c
+++ substringSearch_MPI.c
@@ -14,7 +14,7 @@ void main(int argc, char **argv) {
   char *buf;
   MPI_Offset filesize;
   MPI_File file_in;
-  int bufsize, rest;
+  int bufsize, rest, n;
 
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD,&myrank);
@@ -27,16 +27,25 @@ void main(int argc, char **argv) {
   rest = filesize%size;
 
   if(myrank == 0) {
+
+    printf("\n\nInserire lunghezza sequenza: ");
+    scanf("%d",&n);
+
+    for(int i=1; i<size; i++)
+      MPI_Send(&n, 1, MPI_INT, i, 555, MPI_COMM_WORLD);
+
     printf("filesize : %li, rest : %d\n\n",filesize,rest);
     buf = (char *) malloc((bufsize+rest)*sizeof(char));
-    MPI_File_set_view(file_in, myrank*bufsize*sizeof(char)+myrank*sizeof(char), MPI_CHAR, MPI_CHAR, "native", MPI_INFO_NULL);
+    MPI_File_set_view(file_in, myrank*bufsize*sizeof(char), MPI_CHAR, MPI_CHAR, "native", MPI_INFO_NULL);
     MPI_File_read(file_in, buf, bufsize+rest, MPI_CHAR, &status);
 
   }else {
 
-    buf = (char *) malloc((bufsize)*sizeof(char));
-    MPI_File_set_view(file_in, myrank*bufsize*sizeof(char)+rest*sizeof(char), MPI_CHAR, MPI_CHAR, "native", MPI_INFO_NULL);
-    MPI_File_read(file_in, buf, bufsize, MPI_CHAR, &status);
+    MPI_Recv(&n, 1, MPI_INT, 0, 555, MPI_COMM_WORLD, &status);
+
+    buf = (char *) malloc((bufsize+n)*sizeof(char));
+    MPI_File_set_view(file_in, myrank*bufsize*sizeof(char)+rest*sizeof(char)-n*sizeof(char), MPI_CHAR, MPI_CHAR, "native", MPI_INFO_NULL);
+    MPI_File_read(file_in, buf, bufsize+n, MPI_CHAR, &status);
 
 
   }
