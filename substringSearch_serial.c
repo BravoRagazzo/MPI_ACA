80a5cc04796e985a08536667386794f01b67d766
Diego <diego.mastella01@universitadipavia.it>
2022-12-28 11:15:36 +0100

    fixed serial algorithm (timestamp check added)

--- substringSearch_serial.c
+++ substringSearch_serial.c
@@ -1,10 +1,13 @@
+#define _LARGEFILE64_SOURCE
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
-#define DIM 2500
+#define DIM 5500000
 
-#define NAME1 "genoma-test.txt"
+#define NAME1 "b.txt"
 #define NAME2 "seq.txt"
 
 #define B 101
@@ -18,7 +21,7 @@ int main(int argc, char **argv) {
   char *txt, *seq;
   txt = read_file(NAME1);
 
-  printf("----- TESTO -----\n%s\n\n",txt);
+  //printf("----- TESTO -----\n%s\n\n",txt);
 
   if(argc == 1) {
     seq = read_file(NAME2);
@@ -33,13 +36,22 @@ int main(int argc, char **argv) {
 printf("----- SEQUENZA -----\n%s\n\n",seq);
 
   int *addr;
+
+  unsigned long timee, times = time(NULL);
+
+
   addr = rabin_karp(txt,seq);
+
+  timee = time(NULL);
+
+
   printf("\nseq found %d times\n", addr[0]);
-  int i=1;
-  while(*(addr+i) != -1) {
+
+  for(int i=1; i<=addr[0]; i++){
     printf("n.%d at: %d\n", i, addr[i]);
-    i++;
   }
+
+  printf("\n\ntime: %lu",timee-times);
 
   free(seq);
   free(addr);
@@ -52,28 +64,27 @@ printf("----- SEQUENZA -----\n%s\n\n",seq);
 int *rabin_karp(char *txt, char*seq) {
 
   int *ret;
-  ret = malloc(strlen(txt)*sizeof(int));
-  for(int i=0;i<strlen(txt);i++)
-    ret[i] = -1;
+  ret = malloc(2*sizeof(int));
   char buf[strlen(seq)];
   buf[strlen(seq)] = '\0';
+
 
   strncpy(buf,txt,sizeof(buf));
 
   int seqhash = 0;
   for(int i=0; i<strlen(seq); i++)
     seqhash += *(seq+i)*(B^(strlen(seq)-i));
-  printf("seqhash: %d\n",seqhash);
+  //printf("seqhash: %d\n",seqhash);
 
   int j = 0;
-  for(int i=0;i<strlen(txt)-sizeof(buf);i++) {
-    strncpy(buf,txt+i,sizeof(buf));
-    printf("\ntratto analizzato: %s\n",buf);
+  for(int i=0;i<strlen(txt)-strlen(buf);i++) {
+    //printf("%d\n",i);
+    strncpy(buf,txt+i,strlen(buf));
+    //printf("\ntratto analizzato: %s\n",buf);
     if(hash(buf) == seqhash) {
       ret[j+1] = i;
       j++;
-    } else {
-      continue;
+      ret = realloc(ret,(j+2)*sizeof(int));
     }
   }
 
@@ -91,7 +102,7 @@ int hash(char *pattern) {
     hash += *(pattern+i)*(B^(dim-i));
 
   }
-  printf("hash: %d\n",hash);
+  //printf("hash: %d\n",hash);
   return hash;
 
 }
@@ -100,11 +111,12 @@ char *read_file(char file_name[]) {
 
   FILE *file_in;
   char *buf;
-  file_in = fopen(file_name,"r");
+  file_in = fopen64(file_name,"rb");
   buf = (char*) malloc(DIM*sizeof(char));
 
-  while(fgets(buf,DIM,file_in))
+  while(fread(buf,sizeof(char),DIM,file_in)>0)
     ;
+
 
   fclose(file_in);
 
