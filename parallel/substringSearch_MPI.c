aa39490908d55fa4400a8d1e70d31cb41052bb4b
Diego <diego.mastella01@universitadipavia.it>
2023-01-24 11:21:48 +0100

    added scatter and bcast functions

--- parallel/substringSearch_MPI.c
+++ parallel/substringSearch_MPI.c
@@ -8,8 +8,8 @@ char *read_text(MPI_File file, int bufsize, int rest, int rank, int M, MPI_Statu
 char *read_pat(char file_name[]);
 int rabin_karp(char *pat, char *txt, int rank, int rest);
 
-#define NAME1 "../dataset/genoma1.txt"
-#define NAME2 "../dataset/seq.txt"
+#define NAME1 "./genoma-test.txt"
+#define NAME2 "./seq.txt"
 #define Q 101     // Big prime number
 #define D 256     // Number of characters in the alphabet
 
@@ -17,6 +17,7 @@ void main(int argc, char **argv) {
 
   MPI_Status status;
   int myrank, size;
+  int buf_rest_array[2], M_send;
 
   MPI_Offset N;
   MPI_File file_in;
@@ -60,22 +61,24 @@ void main(int argc, char **argv) {
     fflush(stdout);
     printf("NUMERO DI CPU UTILIZZATE : %d\n",newsize);
 
-    int send[3];
-    send[1] = bufsize;
-    send[2] = rest;
+    int send[2];
+    buf_rest_array[0] = bufsize;
+    buf_rest_array[1] = rest;
+    int m[size];
 
-    for(int i=1; i<size; i++) {
+    printf("\npr%d, bufsize: %d, rest; %d", myrank, bufsize, rest);
+
+    for(int i=0; i<size; i++) {
       if(i < newsize){
-        send[0]=M;
+        m[i]=M;
       }else{
-        send[0]=0;
+        m[i]=0;
       }
-
-      MPI_Send(send, 3, MPI_INT, i, 555, MPI_COMM_WORLD);
     }
 
-    for(int i=1; i<newsize; i++)
-      MPI_Send(pat,M, MPI_CHAR, i, 0, MPI_COMM_WORLD);
+    MPI_Scatter(m, 1, MPI_INT, &M_send, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(pat, M, MPI_CHAR, 0, MPI_COMM_WORLD);
+    MPI_Bcast(buf_rest_array, 2, MPI_INT, 0, MPI_COMM_WORLD);
 
     char *buf;
     buf = read_text(file_in, bufsize + rest, rest, myrank, 1, status);
@@ -85,6 +88,7 @@ void main(int argc, char **argv) {
 
     free(buf);
     free(pat);
+    MPI_Barrier(MPI_COMM_WORLD);
 
     for(i = 1; i<newsize; i++) {
       MPI_Recv(&tmp, 1, MPI_INT, i, 111, MPI_COMM_WORLD, &status);
@@ -92,26 +96,28 @@ void main(int argc, char **argv) {
     }
 
     fflush(stdout);
+
     printf("SEQUENCE FOUND %d TIMES\n",freq);
 
-  }else {
+  } else{
     char *pat;
-    int M, bufsize,rest,freq;
+    int bufsize,rest,freq;
 
-    int recv[3];
-    MPI_Recv(recv, 3, MPI_INT, 0, 555, MPI_COMM_WORLD, &status);
+    MPI_Scatter(NULL, 1, MPI_INT, &M_send, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-    M = recv[0];
-    bufsize = recv[1];
-    rest = recv[2];
+    if(M_send != 0) {
+      pat = malloc(M_send*sizeof(char));
+      MPI_Bcast(pat, M_send, MPI_CHAR, 0, MPI_COMM_WORLD);
 
-    if(M != 0) {
-      pat = malloc(M*sizeof(char));
-      MPI_Recv(pat, M, MPI_CHAR, 0, 0, MPI_COMM_WORLD, &status);
-      *(pat+M)='\0';
+      *(pat+M_send)='\0';
+
+      MPI_Bcast(buf_rest_array, 2, MPI_INT, 0, MPI_COMM_WORLD);
+
+      bufsize = buf_rest_array[0];
+      rest = buf_rest_array[1];
 
       char *buf;
-      buf = read_text(file_in, bufsize, rest, myrank, M, status);
+      buf = read_text(file_in, bufsize, rest, myrank, M_send, status);
 
       freq = rabin_karp(pat,buf,myrank,rest);
 
@@ -119,6 +125,7 @@ void main(int argc, char **argv) {
       free(buf);
       free(pat);
 
+      MPI_Barrier(MPI_COMM_WORLD);
       MPI_Send(&freq, 1, MPI_INT, 0, 111, MPI_COMM_WORLD);
     }
 
@@ -193,10 +200,8 @@ int rabin_karp(char *pat, char *txt, int rank, int rest){
 
         if(j == M){
           fflush(stdout);
-          fprintf(stdout,"\033[0;32m Pattern found at index %d\n\033[0m", rank == 0 ? i : i+(N-(M-1)+rest)-M+1+((rank-1)*(N-M+1)));
-
-          // printf("\033[0;32m Pattern found at index %d\n\033[0m", rank == 0 ? i : i+(N-(M-1)+rest)-M+1+((rank-1)*(N-M+1)));
-
+          //fprintf(stdout,"\033[0;32m Pattern found at index %d\n\033[0m", rank == 0 ? i : i+(N-(M-1)+rest)-M+1+((rank-1)*(N-M+1)));
+          printf("\033[0;32m Pattern found at index %d\n\033[0m", rank == 0 ? i : i+(N-(M-1)+rest)-M+1+((rank-1)*(N-M+1)));
           freq++;
         }
       }
